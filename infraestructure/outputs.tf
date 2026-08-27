704761d87cc160ceca7474084667ee374ae9c7ff
Alejorodazz <alejandrorodas003@gmail.com>
2026-08-27 00:32:55 -0600

    Build: Creación de infraestructura cloud

---
+++ infraestructure/outputs.tf
@@ -0,0 +1,14 @@
+output "server_demo_public-ip" {
+    description = "IP pública de la instancia servidor demo"
+    value = aws_instance.server_demo.public_ip
+}
+
+output "server_demo_type-instance" {
+    description = "Tipo de instancia de mi EC2"
+    value = aws_instance.server_demo.instance_type
+}
+
+output "VPC_main_block-CDIR" {
+    description = "Descripción de bloque CDIR aplicadas a VPC"
+    value = aws_vpc.main.cidr_block
+}
